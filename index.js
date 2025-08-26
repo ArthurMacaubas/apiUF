d8a18d0cd6d244bf41477c624dce84510c1d8327
Arthur Pietro <75856761+ArthurMacaubas@users.noreply.github.com>
2025-08-26 07:49:34 -0400

    a

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 import colecaoUf from "./dados/dados.js";
 import express from "express";
 
-const ip = 1982
+const ip = 1982;
 
 const app = express();
 
 app.get("/ufs", (req, res) => {
-    res.json(colecaoUf)
+  res.json(colecaoUf);
 });
 
 app.get("/ufs/:iduf", (req, res) => {
-    const idUf = parseInt(req.params.iduf);
-    const uf = colecaoUf.find(u => u.id == idUf);
-    res.json(uf);
+  const idUf = parseInt(req.params.iduf);
+  let menssagemErro = "";
+  let uf;
+
+  if (!(isNaN(idUf))) {
+    uf = colecaoUf.find(u => u.id === idUf);
+    if (!uf) {
+      menssagemErro = "UF não encontrada";
+    } else {
+      menssagemErro = "Requisição inválida";
+    }
+
+    if (uf) {
+      res.json(uf);
+    } else {
+      res.status(404).json({ "erro": menssagemErro });
+    }
+  }
 });
 
 app.listen(ip, () => {
-    console.log('Servidor sendo iniciado: http://localhost:' + ip + "/ufs/")
-})
+  console.log("Servidor iniciado em: http://localhost:" + ip + "/ufs/");
+});
